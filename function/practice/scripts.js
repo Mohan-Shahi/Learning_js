f2092b4e9e6408abd4b7dcf98b95d525227f164a
adidaplays <pmdmoonton470@gmail.com>
2026-04-23 07:20:18 +0545

    JavaScript learning function update

--- function/practice/scripts.js
+++ function/practice/scripts.js
@@ -1,10 +1,26 @@
-function Check_even_odd(number){
-    if(number % 2 === 0){
-        return `${number} is even`
+// function Check_even_odd(number){
+//     if(number % 2 === 0){
+//         return `${number} is even`
+//     }
+//     else{
+//         return `${number} is odd`
+//     }
+// }
+// let result = Check_even_odd(5)
+// console.log(result)
+
+
+
+function largest_number(n1,n2){
+    if(n1===n2){
+        return `Both ${n1} and ${n2} are equal`
     }
-    else{
-        return `${number} is odd`
+    else if (n1 > n2 ){
+        return `${n1} is greater then ${n2}`;
+    }
+    else {
+        return `${n2} is greater then ${n1}`
     }
 }
-let result = Check_even_odd(5)
-console.log(result)
+let result = largest_number(6,2);
+console.log(result);
